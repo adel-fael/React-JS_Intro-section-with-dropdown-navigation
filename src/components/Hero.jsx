1ea0f169e1f814c2e6e39231c8b1facbf4d81395
Adel-Fael <new19.adel@gmail.com>
2022-10-19 12:08:03 +0300

    Fixing some errors

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,21 +23,21 @@ const Hero = () => {
         />
       </div>
 
-      <div className=" mobile:flex flex-col justify-between pl-2 pr-2">
-        <div className="mt-6 space-y-16 w-[35ch] mx-auto mobile:w-[50ch]">
+      <div role="article" className="mobile:flex flex-col justify-between pl-2 pr-2">
+        <article className="mt-6 space-y-16 w-[35ch] mx-auto mobile:w-[50ch]">
           <h1 className="text-4xl font-bold text-center mobile:text-left mobile:text-[4.2rem] mobile:leading-[5rem] ">
             Make Remote Work
           </h1>
-          <h4 className=" text-center  font-medium text-gray-500 mobile:text-left">
+          <h2 className=" text-center  font-medium text-gray-500 mobile:text-left">
             Get your team in sync, no matter your location. Streamline
             processes, create team rituals, and watch productivity soar.
-          </h4>
+          </h2>
           <div className="flex justify-center mt-6 mobile:justify-start">
             <button className=" bg-black text-white font-medium rounded-xl pl-5 pr-5 pt-2 pb-2 border-2 border-black button-state  mobile:pl-7 mobile:pr-7 mobile:pt-3 mobile:pb-3">
               Learn More
             </button>
           </div>
-        </div>
+        </article>
 
         <ul className="flex justify-around p-4 mt-5 ">
           <li>
