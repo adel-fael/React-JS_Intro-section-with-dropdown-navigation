191676db93e38623246d3f89ba78b3bfa7eb9abd
Adel-Fael <new19.adel@gmail.com>
2022-10-19 12:01:34 +0300

    fixing Navbar.jsx to fit the semantic html

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,10 +28,10 @@ const Hero = () => {
           <h1 className="text-4xl font-bold text-center mobile:text-left mobile:text-[4.2rem] mobile:leading-[5rem] ">
             Make Remote Work
           </h1>
-          <h3 className=" text-center  font-medium text-gray-500 mobile:text-left">
+          <h4 className=" text-center  font-medium text-gray-500 mobile:text-left">
             Get your team in sync, no matter your location. Streamline
             processes, create team rituals, and watch productivity soar.
-          </h3>
+          </h4>
           <div className="flex justify-center mt-6 mobile:justify-start">
             <button className=" bg-black text-white font-medium rounded-xl pl-5 pr-5 pt-2 pb-2 border-2 border-black button-state  mobile:pl-7 mobile:pr-7 mobile:pt-3 mobile:pb-3">
               Learn More
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,34 +5,39 @@ import Sidebar from './SideBar'
 const Navbar = () => {
   return (
     <nav className="m-4">
-      <ul className="flex justify-between  p-4 ">
-        <div className="flex">
+      <div className="flex justify-between p-4">
+        <ul className="flex">
           <li>
             <img src={logo} alt="snap logo" />
           </li>
           <li className="hidden mobile:block">
             <Dropdown />
           </li>
-        </div>
-        <li className="hidden mobile:block">
-          <a
-            href="#"
-            className="text-gray-500 text-lg  hover:text-black  transition duration-400 px-10 "
-          >
-            Login
-          </a>
-          <a
-            href="#"
-            className="text-gray-500 text-lg border-2 rounded-xl border-gray-400 hover:text-black  hover:border-black transition duration-400 px-4 py-2"
-          >
-            Register
-          </a>
-        </li>
+        </ul>
 
-        <li className="block mobile:hidden">
-          <Sidebar />
-        </li>
-      </ul>
+        <ul>
+          <li className="hidden mobile:block">
+            <a
+              href="#"
+              className="text-gray-500 text-lg  hover:text-black  transition duration-400 px-10 "
+            >
+              Login
+            </a>
+            <a
+              href="#"
+              className="text-gray-500 text-lg border-2 rounded-xl border-gray-400 hover:text-black  hover:border-black transition duration-400 px-4 py-2"
+            >
+              Register
+            </a>
+          </li>
+        </ul>
+
+        
+          <div className="block mobile:hidden">
+            <Sidebar />
+          </div>
+
+      </div>
     </nav>
   )
 }
